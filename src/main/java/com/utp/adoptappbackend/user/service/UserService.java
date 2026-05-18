c26293dcc65fba68a09e35c5fb36b0f84eeea3f9
tunita277 <ariesycromoeirl@gmail.com>
2026-05-18 17:15:37 -0500

    Refactor: Eliminar métodos forgotPassword() y resetPassword() de interfaz UserService

--- src/main/java/com/utp/adoptappbackend/user/service/UserService.java
+++ src/main/java/com/utp/adoptappbackend/user/service/UserService.java
@@ -3,9 +3,7 @@ package com.utp.adoptappbackend.user.service;
 import com.utp.adoptappbackend.common.model.PageResponse;
 import com.utp.adoptappbackend.common.model.enumeration.Role;
 import com.utp.adoptappbackend.user.model.dto.AuthRegisterRequest;
-import com.utp.adoptappbackend.user.model.dto.ForgotPasswordRequest;
 import com.utp.adoptappbackend.user.model.dto.LoginResponse;
-import com.utp.adoptappbackend.user.model.dto.ResetPasswordRequest;
 import com.utp.adoptappbackend.user.model.dto.UserRequest;
 import com.utp.adoptappbackend.user.model.dto.UserResponse;
 import com.utp.adoptappbackend.user.model.dto.UserUpdateRequest;
@@ -15,8 +13,6 @@ public interface UserService {
     LoginResponse login(AuthRegisterRequest request);
     UserResponse findById(Long id);
     UserResponse update(Long id, UserUpdateRequest request);
-    void forgotPassword(ForgotPasswordRequest request);
-    void resetPassword(ResetPasswordRequest request);
     PageResponse<UserResponse> findAll(int page, int size);
     PageResponse<UserResponse> findByRole(Role role, int page, int size);
     UserResponse deactivateUser(Long id);
